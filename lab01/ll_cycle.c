8d816ebfe586932c0b2eaa0cc80cdb4ff0b7eed9
scommune <ffeeff@126.com>
2023-03-02 04:48:06 +0000

    完成lab01，并且尝试在codespace环境里使用git

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    //test codespace's git
+    node* fp,sp=head;
+    while(fp!=NULL&& fp->next!=NULL){
+        fp=fp->next->next;
+        sp=sp->next;
+        if(fp==sp){
+            return 1;
+        }
+    }
     return 0;
 }
